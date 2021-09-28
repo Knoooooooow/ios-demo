94e1a490f33e5d8db8f0e710059aa3ee268222fb
zd zhang <brianz.d.zhang@zds-MacBook-Pro.local>
2021-09-28 11:58:04 +0800

    next step : Handling User Input

--- Blog/Views/LandmarkList.swift
+++ Blog/Views/LandmarkList.swift
@@ -1,27 +1,21 @@
-//
-//  LandmarkList.swift
-//  Blog
-//
-//  Created by zd zhang on 2021/9/26.
-//
-
 import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
         NavigationView {
             List(landmarks) { landmark in
-                NavigationLink(destination: LandmarkDetail()) {
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
                     LandmarkRow(landmark: landmark)
                 }
             }
             .navigationTitle("Landmarks")
         }
-   }
+    }
 }
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
         LandmarkList()
+            .previewDevice(PreviewDevice(rawValue: "iPhone SE (2nd generation)"))
     }
 }
